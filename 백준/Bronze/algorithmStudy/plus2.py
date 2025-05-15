14a9a69c9b2f49765d8a80716a4883dbbb4bb914
Kimbang1 <kbhsun123@gmail.com>
2025-05-16 02:02:31 +0900

    10951, 10952 풀이

--- 백준/Bronze/algorithmStudy/plus2.py
+++ 백준/Bronze/algorithmStudy/plus2.py
@@ -6,9 +6,13 @@
 
 # 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
 
+
 T = int(input())
 
-for _ in range(T):
-    A, B = map(int,input().split())
-    print(A + B)
+results = []
 
+for _ in range(T):
+    A,B = map(int,input().split())
+    results.append(str(A+B))
+
+print('\n'.join(results))
---
+++ 백준/Bronze/algorithmStudy/plus3.py
@@ -0,0 +1,24 @@
+#두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오
+
+#입력: 입력은 여러 개의 테스트 케이스로 이루어져 있다.
+#각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다(0<A,B<10)
+
+#출력: 각 테스트 케이스마다 A+B를 출력한다.
+
+import sys
+
+results = []
+
+def main():
+    for line in sys.stdin:
+
+        if not line.strip():
+            continue
+
+        A,B = map(int,line.split())
+        results.append(str(A+B))
+
+    print('\n'.join(results))
+
+if __name__ == "__main__":
+    main()
