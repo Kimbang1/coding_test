146f5e0d29792d0dca6ee9d7ce870ff3da1e9554
Kimbang1 <arto135@naver.com>
2025-05-20 01:05:48 +0900

    입출력 문제 풀이

--- 백준/Bronze/algorithmStudy/Output/Main.java
+++ 백준/Bronze/algorithmStudy/Output/Main.java
@@ -60,21 +60,46 @@ public class Main {
     //     }
     // }
 
-    //2741번 문제
-    public static void main(String[] args) throws IOException{
+    // //2741번 문제
+    // public static void main(String[] args) throws IOException{
      
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
+    //     int N = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <=N; i++){
-            bw.write(i + "\n");
-        }
+    //     for (int i = 1; i <=N; i++){
+    //         bw.write(i + "\n");
+    //     }
 
-        bw.flush();
+    //     bw.flush();
 
-        bw.close();
-        br.close();
+    //     bw.close();
+    //     br.close();
+    // }
+
+    //2742번 문제
+//     문제
+// 자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+
+// 출력
+// 첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+    public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+
+    for(int i = N; i>= 1; i--){
+        bw.write(i + "\n");
     }
+    bw.flush();
+
+    bw.close();
+    br.close();
+    }
+
 }
