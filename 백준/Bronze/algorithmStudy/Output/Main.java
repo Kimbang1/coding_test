0889e5ca86713df8a7cd10e4d84da8ee145a8b25
Kimbang1 <arto135@naver.com>
2025-05-29 18:32:55 +0900

    별 빈공간 찍기

--- 백준/Bronze/algorithmStudy/Output/Main.java
+++ 백준/Bronze/algorithmStudy/Output/Main.java
@@ -62,7 +62,7 @@ public class Main {
 
     // //2741번 문제
     // public static void main(String[] args) throws IOException{
-     
+    
     //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     //     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
